f42b8d148dd27603a0f2eb960a037392252ddda4
Veronica Ramirez <veronica.ramirez2020@outlook.com>
2023-10-05 07:47:53 -0400

    image

--- controllers/image.js
+++ controllers/image.js
@@ -4,7 +4,7 @@ const returnClarifaiRequestOptions = (imageUrl) => {
   const PAT = '110b9326dd5c47a9b254093ad1fc205c';
   const USER_ID = 'wan0qa9g44e9';
   const APP_ID = 'face-detection';
-  const MODEL_ID = 'face-detection';
+  const MODEL_ID = '6dc7e46bc9124c5c8824be4822abe105';
   const IMAGE_URL = imageUrl;
   const raw = JSON.stringify({
     user_app_id: {
@@ -40,9 +40,9 @@ const handleApiCall = (req, res) => {
     'https://api.clarifai.com/v2/models/' + 'face-detection' + '/outputs',
     returnClarifaiRequestOptions(req.body.input)
   )
-    .then((response) => response.text())
-    .then((result) => {
-      res.json(result);
+    .then((response) => response.json())
+    .then((data) => {
+      res.json(data);
     })
     .catch((error) =>
       res.status(500).json('Unable to communicate with API', error)
