1cca96f8d86b1769da47f9a95763f2be3bca6fb2
Veronica Ramirez <veronica.ramirez2020@outlook.com>
2023-10-06 15:50:10 -0400

    image

--- controllers/image.js
+++ controllers/image.js
@@ -1,55 +1,23 @@
 const Clarifai = require('clarifai');
+const fetch = require('node-fetch');
 
+const API_KEY = '110b9326dd5c47a9b254093ad1fc205c';
 const MODEL_ID = 'face-detection';
 const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
 
-const returnClarifaiRequestOptions = (imageUrl) => {
-  const PAT = '110b9326dd5c47a9b254093ad1fc205c';
-  const USER_ID = 'wan0qa9g44e9';
-  const APP_ID = 'face-detection';
-
-  const IMAGE_URL = imageUrl;
-  const raw = JSON.stringify({
-    user_app_id: {
-      user_id: USER_ID,
-      app_id: APP_ID,
-    },
-    inputs: [
-      {
-        data: {
-          image: {
-            url: IMAGE_URL,
-          },
-        },
-      },
-    ],
-  });
-
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      Authorization: 'Key ' + PAT,
-    },
-    body: raw,
-  };
-  console.log(requestOptions);
-  return requestOptions;
-};
+const app = new Clarifai.App({ apiKey: API_KEY });
 
 const handleApiCall = (req, res) => {
-  const requestOptions = returnClarifaiRequestOptions(req.body.input);
-  fetch(
-    `https://api.clarifai.com/v2/models/${MODEL_ID}/versions/${MODEL_VERSION_ID}/outputs`,
-    requestOptions
-  )
+  app.models
+    .predict(MODEL_ID, MODEL_VERSION_ID, req.body.input)
     .then((data) => {
       res.json(data);
       console.log('Clarifai API response', data);
     })
-    .catch((error) =>
-      res.status(500).json({ error: 'Unable to communicate with API' })
-    );
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: 'Unable to communicate with API' });
+    });
 };
 
 const handleImage = (req, res, db) => {
