e1f7112983cdcd832ea75b5de7662d00d63ebaf6
Veronica Ramirez <veronica.ramirez2020@outlook.com>
2023-10-06 19:56:29 -0400

    image

--- controllers/image.js
+++ controllers/image.js
@@ -5,13 +5,26 @@ const fetch = require('node-fetch');
 const handleApiCall = (req, res, imageUrl) => {
   const PAT = '110b9326dd5c47a9b254093ad1fc205c';
   const USER_ID = 'wan0qa9g44e9';
-  const APP_ID = 'main';
+  const APP_ID = 'face-detection';
   const MODEL_ID = 'face-detection';
   const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+  const IMAGE_URL = imageUrl;
 
-  const apiUrl = `https://api.clarifai.com/v2/models/${MODEL_ID}/versions/${MODEL_VERSION_ID}/outputs?url=${encodeURIComponent(
-    imageUrl
-  )}`;
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
 
   const requestOptions = {
     method: 'POST',
@@ -19,20 +32,20 @@ const handleApiCall = (req, res, imageUrl) => {
       Accept: 'application/json',
       Authorization: 'Key ' + PAT,
     },
+    body: raw,
   };
 
-  fetch(apiUrl, requestOptions)
-    .then((response) => response.json())
-    .then((result) => {
-      console.log(result);
-      res.json(result);
-    })
-    .catch((error) => {
-      console.log('error', error);
-      res.status(400).json('Error processing the image');
-    });
+  const response = fetch(
+    'https://api.clarifai.com/v2/models/' +
+      MODEL_ID +
+      '/versions/' +
+      MODEL_VERSION_ID +
+      '/outputs',
+    requestOptions
+  );
+  const data = response.json();
+  return data;
 };
-
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users')
