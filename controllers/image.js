ed93b1ef8a2b0415d36437f29cc2ed7d52236c7a
Veronica Ramirez <veronica.ramirez2020@outlook.com>
2023-10-06 16:45:06 -0400

    image

--- controllers/image.js
+++ controllers/image.js
@@ -1,25 +1,52 @@
 const Clarifai = require('clarifai');
 
-const API_KEY = '110b9326dd5c47a9b254093ad1fc205c';
-const MODEL_ID = 'face-detection';
-const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
-
-const clarifaiApp = new Clarifai.App({ apiKey: API_KEY });
-
 const handleApiCall = (req, res) => {
-  clarifaiApp.models
-    .predict(MODEL_ID, MODEL_VERSION_ID, input)
-    .then((data) => {
-      res.json(data);
-      console.log('Clarifai API response', data);
-    })
-    .catch((err) => {
-      console.error(err);
-      res
-        .status(500)
-        .json({ error: 'Unable to communicate with Clarifai API' });
-    });
-};
+  const PAT = '110b9326dd5c47a9b254093ad1fc205c';
+  const USER_ID = 'wan0qa9g44e9';
+  const APP_ID = 'face-detection';
+  const MODEL_ID = 'face-detection';
+  const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+
+  const IMAGE_URL = this.state.input;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Key ' + PAT,
+    },
+    body: raw,
+  };
+
+  fetch(
+    'https://api.clarifai.com/v2/models/' +
+      MODEL_ID +
+      '/versions/' +
+      MODEL_VERSION_ID +
+      '/outputs',
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => {
+  console.log(result);
+})
+.catch((err) => res.status(400).json('unable to work with API'));
 
 const handleImage = (req, res, db) => {
   const { id } = req.body;
