c2fca7f46ac911c12a460ebe4e574a4d4621e06a
Veronica Ramirez <veronica.ramirez2020@outlook.com>
2023-10-06 19:29:34 -0400

    image

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,6 @@
 const Clarifai = require('clarifai');
 const { response } = require('express');
 const fetch = require('node-fetch');
-const { ClarifaiStub, grpc } = require('clarifai-nodejs-grpc');
 
 const handleApiCall = (req, res, imageUrl) => {
   const PAT = '110b9326dd5c47a9b254093ad1fc205c';
@@ -11,44 +10,44 @@ const handleApiCall = (req, res, imageUrl) => {
   const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
   const IMAGE_URL = imageUrl;
 
-  const stub = ClarifaiStub.grpc();
-
-  const metadata = new grpc.Metadata();
-  metadata.set('authorization', 'Key ' + PAT);
-
-  stub.PostModelOutputs(
-    {
-      user_app_id: {
-        user_id: USER_ID,
-        app_id: APP_ID,
-      },
-      model_id: MODEL_ID,
-      version_id: MODEL_VERSION_ID,
-      inputs: [
-        { data: { image: { url: IMAGE_URL, allow_duplicate_url: true } } },
-      ],
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
     },
-    metadata,
-    (err, response) => {
-      if (err) {
-        throw new Error(err);
-      }
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
 
-      if (response.status.code !== 10000) {
-        throw new Error(
-          'Post model outputs failed, status: ' + response.status.description
-        );
-      }
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Key ' + PAT,
+    },
+    body: raw,
+  };
 
-      const output = response.outputs[0];
-
-      console.log('Predicted concepts:');
-      for (const concept of output.data.concepts) {
-        console.log(concept.name + ' ' + concept.value);
-      }
-    }
-  );
+  fetch(
+    'https://api.clarifai.com/v2/models/' +
+      MODEL_ID +
+      '/versions/' +
+      MODEL_VERSION_ID +
+      '/outputs',
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log('error', error));
 };
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users')
