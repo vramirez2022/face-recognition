609fb64c316d622ff2ac6df6cd34b00dd9e9adde
Veronica Ramirez <veronica.ramirez2020@outlook.com>
2023-10-06 16:00:47 -0400

    image

--- controllers/image.js
+++ controllers/image.js
@@ -1,22 +1,23 @@
 const Clarifai = require('clarifai');
-const fetch = require('node-fetch');
 
 const API_KEY = '110b9326dd5c47a9b254093ad1fc205c';
 const MODEL_ID = 'face-detection';
 const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
 
-const app = new Clarifai.App({ apiKey: API_KEY });
+const clarifaiApp = new Clarifai.App({ apiKey: API_KEY });
 
 const handleApiCall = (req, res) => {
-  app.models
-    .predict(MODEL_ID, MODEL_VERSION_ID, req.body.input)
+  clarifaiApp.models
+    .predict(MODEL_ID, MODEL_VERSION_ID, input)
     .then((data) => {
       res.json(data);
       console.log('Clarifai API response', data);
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).json({ error: 'Unable to communicate with API' });
+      res
+        .status(500)
+        .json({ error: 'Unable to communicate with Clarifai API' });
     });
 };
 
