f233a02a788468d775264237579454910603404d
Veronica Ramirez <veronica.ramirez2020@outlook.com>
2023-10-10 07:43:36 -0400

    'image'

--- controllers/image.js
+++ controllers/image.js
@@ -2,14 +2,12 @@ const Clarifai = require('clarifai');
 const { response } = require('express');
 const fetch = require('node-fetch');
 
-const handleApiCall = (req, res, imageUrl) => {
+const returnClarifaiRequestOptions = (imageUrl) => {
   const PAT = '110b9326dd5c47a9b254093ad1fc205c';
   const USER_ID = 'wan0qa9g44e9';
   const APP_ID = 'face-detection';
   const MODEL_ID = 'face-detection';
-  const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
   const IMAGE_URL = imageUrl;
-
   const raw = JSON.stringify({
     user_app_id: {
       user_id: USER_ID,
@@ -34,30 +32,22 @@ const handleApiCall = (req, res, imageUrl) => {
     },
     body: raw,
   };
+  console.log(requestOptions);
+  return requestOptions;
+};
 
+const handleApiCall = (req, res) => {
+  console.log('imageurl: ', req.body.input);
   fetch(
-    'https://api.clarifai.com/v2/models/' +
-      MODEL_ID +
-      '/versions/' +
-      MODEL_VERSION_ID +
-      '/outputs',
-    requestOptions
+    'https://api.clarifai.com/v2/models/' + 'face-detection' + '/outputs',
+    returnClarifaiRequestOptions(req.body.input)
   )
-    .then((response) => {
-      if (response.status === 200) {
-        return response.json();
-      } else {
-        throw new Error('Server responded with an error');
-      }
-    })
+    .then((response) => response.json())
     .then((data) => {
-      console.log('Data from server:', data);
+      console.log('Clarifai API response', data);
       res.json(data);
     })
-    .catch((error) => {
-      console.error('Error:', error);
-      res.status(500).json('Internal Server Error');
-    });
+    .catch((err) => res.status(400).json('unable to work with api'));
 };
 
 const handleImage = (req, res, db) => {
